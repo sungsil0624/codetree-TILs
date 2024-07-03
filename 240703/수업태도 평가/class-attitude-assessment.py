62b68389062faebecc738cd5df84792f38b5d649
준희 <113875098+sungsil0624@users.noreply.github.com>
2024-07-03 22:14:59 +0900

    Title: 수업태도 평가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240703/수업태도 평가/class-attitude-assessment.py
@@ -0,0 +1,31 @@
+import sys
+
+student = {"Bessie": 0, "Elsie": 0, "Daisy": 0, "Gertie": 0, "Annabelle": 0, "Maggie": 0, "Henrietta": 0}
+
+n = int(sys.stdin.readline().strip())
+
+for _ in range(n):
+    name, score = sys.stdin.readline().strip().split()
+    student[name] += int(score)
+
+sorted_student = sorted(student.items(), key=lambda item: item[1])
+
+lowest_score = sorted_student[0][1]
+
+num_lowest_scores = sum(1 for name, score in sorted_student if score == lowest_score)
+
+second_score = sorted_student[num_lowest_scores][1]
+
+num_second_scores = sum(1 for name, score in sorted_student if score == second_score)
+
+score_list = []
+
+for name, score in sorted_student:
+    score_list.append(score)
+
+if num_lowest_scores > 1 and num_second_scores < 2:
+    print(sorted_student[num_lowest_scores][0])
+elif num_second_scores > 1 or len(set(score_list)) == 2:
+    print("Tie")
+else:
+    print(sorted_student[1][0])
