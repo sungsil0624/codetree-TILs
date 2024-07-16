8aa2a6638d7d4de00881dc1266d45954c3549dd5
준희 <113875098+sungsil0624@users.noreply.github.com>
2024-07-16 11:05:52 +0900

    Title: 데이터 비교 2, Time: 143ms, Memory: 30MB, Status: Wrong Answer - CodeTree

---
+++ 240716/데이터 비교 2/date-comparison-2.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+word = {}
+
+for _ in range(n):
+    input_word = sys.stdin.readline().strip()
+
+    word[input_word] = 0
+
+for _ in range(n - 1):
+    parity_word = sys.stdin.readline().strip()
+
+    if parity_word in word.keys():
+        word[parity_word] = 1
+
+for k, v in word.items():
+    if v == 0:
+        print(k)
