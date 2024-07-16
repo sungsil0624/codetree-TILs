c199656ca736b50d4aa091d125fc4458a1e33155
준희 <113875098+sungsil0624@users.noreply.github.com>
2024-07-16 11:06:36 +0900

    Title: 데이터 비교 2, Time: 153ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 240716/데이터 비교 2/date-comparison-2.py
+++ 240716/데이터 비교 2/date-comparison-2.py
@@ -13,7 +13,7 @@ for _ in range(n - 1):
     parity_word = sys.stdin.readline().strip()
 
     if parity_word in word.keys():
-        word[parity_word] = 1
+        word[parity_word] += 1
 
 for k, v in word.items():
     if v == 0:
