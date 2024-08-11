c73fb64e14ad0719bb0e76d5ecc527b93e006674
준희 <113875098+sungsil0624@users.noreply.github.com>
2024-08-11 20:39:48 +0900

    Title: 문자열 검사기, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240811/문자열 검사기/string-inspector.py
+++ 240811/문자열 검사기/string-inspector.py
@@ -8,9 +8,8 @@ input_str = sys.stdin.readline().strip()
 for i in input_str:
     if i == "(":
         q.append(i)
-    elif q and i == ")" and q[-1] == "(":
-        if q:
-            q.pop()
+    elif q and i == ")" and q[-1] == "(":\
+        q.pop()
     elif i == "[":
         q.append(i)
     elif q and q[-1] == "[":
