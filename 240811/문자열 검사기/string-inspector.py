694952298d871422a92c24912b9eb796e7b06f0b
준희 <113875098+sungsil0624@users.noreply.github.com>
2024-08-11 20:38:07 +0900

    Title: 문자열 검사기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240811/문자열 검사기/string-inspector.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+
+q = deque()
+
+input_str = sys.stdin.readline().strip()
+
+for i in input_str:
+    if i == "(":
+        q.append(i)
+    elif i == ")" and q[-1] == "(":
+        if q:
+            q.pop()
+    elif i == "[":
+        q.append(i)
+    elif q and q[-1] == "[":
+        q.pop()
+
+if not q:
+    print(1)
+else:
+    print(0)
